571bb467929d6d39a4a05a31b8ce97cf667375c5
gursimar1997 <gursimar.chopra@mountblue.io>
2018-10-17 12:29:46 +0530

    spiking

--- auc/settings.py
+++ auc/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'datetimepicker',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware'
 ]
 
 ROOT_URLCONF = 'auc.urls'
--- auction/forms.py
+++ auction/forms.py
@@ -1,15 +1,27 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from datetimewidget.widgets import DateTimeWidget
+from datetimepicker.widgets import DateTimePicker
+from .models import auction
 
 
-class createAuction(forms.Form):
-    title = forms.CharField()
-    base_price = forms.IntegerField()
-    start_time = forms.DateTimeField(input_formats=['%Y/%m/%d %H:%M:%S'], widget=forms.DateTimeInput(format='%Y/%m/%d %H:%M:%S'))
-    end_time = forms.DateTimeField(input_formats=['%Y/%m/%d %H:%M:%S'], widget=forms.DateTimeInput(format='%Y/%m/%d %H:%M:%S'))
-    description = forms.CharField(widget=forms.Textarea())
-    location = forms.CharField(max_length=3)
+class createAuction(forms.ModelForm):
+    class Meta:
+        model = auction
+        fields = ['title', 'description', 'start_time', 'end_time', 'location', 'base_price']
+        widgets = {'start_time': forms.DateTimeInput(widget=DateTimePicker(),),
+                   'end_time': forms.DateTimeInput(attrs={'class': 'datetime-input'})}
+
+# class createAuction(forms.Form):
+#     title = forms.CharField()
+#     base_price = forms.IntegerField()
+#     # start_time = forms.DateTimeField(input_formats=['%Y/%m/%d %H:%M:%S'], widget=forms.DateTimeInput(format='%Y/%m/%d %H:%M:%S'))
+#     start_time = forms.DateTimeField(widget=DateTimePicker(),)
+#     end_time = forms.DateTimeField(input_formats=['%Y/%m/%d %H:%M:%S'], widget=forms.DateTimeInput(format='%Y/%m/%d %H:%M:%S'))
+#     description = forms.CharField(widget=forms.Textarea())
+#     location = forms.CharField(max_length=3)
+
 
 
 class confAuction(forms.Form):
