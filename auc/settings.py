67726f2a62a8153cfea466f201ff134a4d5f99a8
Shubham Singh Rajput <shubham.rajput@mountblue.io>
2018-10-17 23:38:56 +0530

    intial ui improvements

--- auc/settings.py
+++ auc/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,11 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'auc/media')
 
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
--- auction/forms.py
+++ auction/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import auction
+from crispy_forms.helper import FormHelper
 
 
 class createAuction(forms.ModelForm):
@@ -14,9 +15,12 @@ class createAuction(forms.ModelForm):
 
     class Meta:
         model = auction
+        helper = FormHelper()
         fields = ['title', 'description', 'start_time', 'end_time', 'location', 'base_price', 'image']
         widgets = {'start_time': forms.DateTimeInput(attrs={'class': 'datetime-input'}),
-                   'end_time': forms.DateTimeInput(attrs={'class': 'datetime-input'})}
+                   'end_time': forms.DateTimeInput(attrs={'class': 'datetime-input'}),
+                   }
+
 
 
 class UserCreateForm(UserCreationForm):
@@ -24,7 +28,14 @@ class UserCreateForm(UserCreationForm):
 
     class Meta:
         model = User
+        helper = FormHelper()
         fields = ("username", "email", "password1", "password2")
+        widgets = {'username': forms.TextInput(attrs={ 'class': 'form-control'}),
+                   'email': forms.TextInput(attrs={ 'class': 'form-control'}),
+                   'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+                   'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+                   }
+
 
     def save(self, commit=True):
         user = super(UserCreateForm, self).save(commit=False)
